a196398aab5e6d450b4379c5ac1bf0a7fb3238c3
v1centebarros <vmabarros@ua.pt>
2023-05-18 11:39:04 +0100

    Add Types from model

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -9,35 +9,35 @@ export const Home: React.FunctionComponent<HomeProps> = (props) => {
 
     const mockProducts: Product[] = [
         {
-            id: 1,
+            id: "1",
             name: "Product 1",
             description: "Description 1",
             price: 100,
             image: "https://picsum.photos/seed/1/400/300",
         },
         {
-            id: 2,
+            id: "2",
             name: "Product 2",
             description: "Description 2",
             image: "https://picsum.photos/seed/2/400/300",
             price: 200,
         },
         {
-            id: 3,
+            id: "3",
             name: "Product 3",
             description: "Description 3",
             image: "https://picsum.photos/seed/3/400/300",
             price: 300,
         },
         {
-            id: 4,
+            id: "4",
             name: "Product 4",
             description: "Description 4",
             image: "https://picsum.photos/seed/4/400/300",
             price: 400,
         },
         {
-            id: 5,
+            id: "5",
             name: "Product 5",
             description: "Description 5",
             image: "https://picsum.photos/seed/5/400/300",
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,104 @@
 export interface Product {
-    id: number;
+    id: string;
     name: string;
     price: number;
     description: string;
     image: string;
+}
+
+export interface Author {
+    id: string;
+    firstName: string;
+    lastName: string;
+    bio: string;
+}
+
+export interface Book {
+    id: string;
+    title: string;
+    author: Author;
+    price: number;
+    discount: number;
+    publisher: string;
+    year: number;
+    pages: number;
+    description: string;
+    imageUrl: string;
+    categores: Category[];
+}
+
+export interface Category {
+    id: string;
+    name: string;
+    slug: string;
+}
+
+export interface Client {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    role: Role;
+}
+
+enum Role {
+    ROLE_ADMIN = "ROLE_ADMIN",
+    ROLE_USER = "ROLE_USER",
+}
+
+export interface Payment {
+    id: string;
+    name: string;
+    cardNumber: number;
+    expirationDate: string;
+    cvv: number;
+}
+
+export interface OrderItem {
+    id: string;
+    book: Book;
+    quantity: number;
+}
+
+export interface OrderStatus {
+    id: string;
+    status: Status;
+    timetamp: string;
+
+
+}
+
+enum Status {
+    SHIPPING = "SHIPPING",
+    DELIVERED = "DELIVERED",
+    AVAILABLE = "AVAILABLE",
+    RETURNED = "RETURNED",
+    FORGOTTEN = "FORGOTTEN",
+}
+
+export interface PickUpService {
+    id: string;
+    name: string;
+    slug: string;
+}
+
+export interface PickUpLocation {
+    id: string;
+    name: string;
+    slug: string;
+    latitude: number;
+    longitude: number;
+    pickUpService: PickUpService;
+}
+
+export interface Order {
+    id: string;
+    client: Client;
+    date: string;
+    payment: Payment;
+    orderItems: OrderItem[];
+    orderStatus: OrderStatus[];
+    pickUpLocation: PickUpLocation;
+    pickUpService: PickUpService;
 }
